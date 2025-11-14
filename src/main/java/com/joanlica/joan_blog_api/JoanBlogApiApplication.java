d8de43c31ab18ef6c649dade3ed10ad85761a9fe
Joanjo17 <joanlica04@hotmail.com>
2025-11-15 00:00:42 +0100

    Initial commit

---
+++ src/main/java/com/joanlica/joan_blog_api/JoanBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.joanlica.joan_blog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoanBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoanBlogApiApplication.class, args);
+	}
+
+}
